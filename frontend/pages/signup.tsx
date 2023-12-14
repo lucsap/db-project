563c4dffa3a90705a17ddb6f8ceadaab21b7ea88
Ana Beatriz Pontes <anabeatrizsantospontes@gmail.com>
2023-12-14 00:27:23 -0300

    Add signup

---
+++ frontend/pages/signup.tsx
@@ -0,0 +1,140 @@
+import { useRouter } from 'next/router';
+import styles from './styles.module.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { useState } from 'react';
+
+export default function SignUp() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [nome, setNome] = useState('');
+    const [sobrenome, setSobrenome] = useState('');
+    const [isSubmiting, setIsSubmiting] = useState(false);
+
+    const notify = () => toast("Dados invalidos !")
+
+    const router = useRouter();
+
+    const handleNameChange = (event: any) => {
+        setNome(event.target.value);
+    };
+
+    const handleSobrenomeChange = (event: any) => {
+        setSobrenome(event.target.value);
+    };
+
+    const handleSenhaChange = (event: any) => {
+        setSenha(event.target.value);
+    };
+
+    const handleEmailChange = (event: any) => {
+        setEmail(event.target.value);
+    };
+
+    const handleSubmit = async (event: any) => {
+        if (isSubmiting) {
+            return;
+        }
+        event.preventDefault();
+
+        const data = {
+            email: email,
+            senha: senha,
+            nome: nome,
+            sobrenome: sobrenome
+        };
+
+        try {
+            setIsSubmiting(true);
+            const response = await fetch('http://localhost:3001/usuarios/cadastro', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            });
+
+
+            if (response.ok) {
+                console.log(response)
+                // const { token } = await response.json();
+                // localStorage.setItem('@token', token);
+                router.push('/home');
+            }
+            else {
+                setIsSubmiting(false);
+                notify();
+            }
+        } catch (error) {
+            setIsSubmiting(false);
+            console.log(error);
+        } finally {
+            setTimeout(() => {
+                setIsSubmiting(false);
+            }, 2000)
+        };
+
+    };
+
+    return (
+        <div className={styles.loginScreen}>
+
+            <div className={styles.screenSty}></div>
+            <div className={styles.personalBox}>
+                <ToastContainer />
+                <h3>Bem vindo!</h3>
+                <h4>
+                    Informe seus dados para se cadastrar na melhor plataforma de livros do Brasil!
+                </h4>
+
+                <form onSubmit={handleSubmit} className={styles.formContainer}>
+                    <div className={styles.formInfos}>
+                        <label className={styles.formLabel}>Nome</label>
+                        <input
+                            id='nome'
+                            className={styles.formInput}
+                            name='nome'
+                            placeholder='Branco'
+                            onChange={handleNameChange}
+                        />
+                    </div>
+                    <div className={styles.formInfos}>
+                        <label className={styles.formLabel}>Sobrenome</label>
+                        <input
+                            id='sobrenome'
+                            className={styles.formInput}
+                            name='sobrenome'
+                            placeholder='Retinto'
+                            onChange={handleSobrenomeChange}
+                        />
+                    </div>
+                    <div className={styles.formInfos}>
+                        <label className={styles.formLabel}>Email</label>
+                        <input
+                            id='email'
+                            className={styles.formInput}
+                            name='email'
+                            type='email'
+                            placeholder='email@email.com'
+                            onChange={handleEmailChange}
+                        />
+                    </div>
+                    <div className={styles.formInfos}>
+                        <label className={styles.formLabel}>Senha</label>
+                        <input
+                            id='senha'
+                            className={styles.formInput}
+                            type='password'
+                            name='senha'
+                            placeholder='********'
+                            onChange={handleSenhaChange}
+                        />
+                    </div>
+                    <div className={styles.btnLogin}>
+                        <button type="submit" className={styles.btnPrimary} disabled={isSubmiting} >
+                            {isSubmiting ? 'Cadastrando...' : 'Cadastrar'}
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
