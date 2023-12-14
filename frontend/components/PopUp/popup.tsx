5bd03554ff644c42c9ddcbdf3e195be299db84a0
Ana Beatriz Santos Pontes <anabeatrizsantospontes@gmail.com>
2023-12-14 13:14:51 -0300

    Add logout

---
+++ frontend/components/PopUp/popup.tsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.css';
+
+import React from 'react';
+
+const PopUp = ({ isOpen, onClose, children } : { children: React.ReactNode }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modal}>
+        <button className={styles.modalClose} onClick={onClose}>X</button>
+          <div className={styles.modalContainer}>
+                {children}
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default PopUp;
