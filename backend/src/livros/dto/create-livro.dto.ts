b322d33c9474c05b7d12d44eb3103d3303c31d30
Barbosa885 <gustavosports7@gmail.com>
2023-12-15 01:47:11 -0300

    dale

--- backend/src/livros/dto/create-livro.dto.ts
+++ backend/src/livros/dto/create-livro.dto.ts
@@ -1,18 +1,26 @@
-import { IsDate, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class CreateLivroDto {
+  @IsString()
+  @IsNotEmpty()
   titulo: string;
+
+  @IsString()
   categoria: string;
+
+  @IsString()
+  @IsOptional()
   descricao: string;
+
+  @IsString()
   localizacao_fisica: string;
 
   @IsString()
   estado_conservacao: string;
 
-  @IsDate()
-  data_aquisicao: Date;
-
   @IsString()
   autor: string;
+
+  @IsString()
   uri_capa_livro: string;
 }
