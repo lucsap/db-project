a6a027a94a3273da3c791b47492acabe7448e751
Barbosa885 <gustavosports7@gmail.com>
2023-12-15 22:26:10 -0300

    fix border on root pages

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { Lexend } from 'next/font/google'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import styles from './globals.module.css'
+import * as React from 'react'
 
 const lexend = Lexend({ 
   subsets: ['latin'],
@@ -14,11 +16,27 @@ export default function MyApp({ Component, pageProps, router }: any) {
   const shouldExcludeHeaderFooter = excludedPaths.includes(router.pathname)
 
   return (
-    <main className={lexend.className}>
-      {!shouldExcludeHeaderFooter && <Header />}
-          <Component {...pageProps} />
-      {!shouldExcludeHeaderFooter && <Footer />}
-    </main>
+      <main className={lexend.className}>
+        <GlobalStyles />
+        {!shouldExcludeHeaderFooter && <Header />}
+            <Component {...pageProps} />
+        {!shouldExcludeHeaderFooter && <Footer />}
+      </main>
   )
 }
 
+function GlobalStyles() {
+    React.useEffect(() => {
+      if (typeof document !== 'undefined') {
+        document.body.classList.add(styles.globalStyles);
+      } 
+
+    return () => {
+      if (typeof document !== 'undefined') {
+        document.body.classList.remove(styles.globalStyles);
+      }
+    };
+  }, [])
+
+  return null;
+}
