9ba377cdedb7dfc41d4dfe5cdd963937378210e2
Barbosa885 <gustavosports7@gmail.com>
2023-12-16 15:07:14 -0300

    adds swagger api documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3001);
 
   useContainer(
     app.select(AppModule), { 
       fallbackOnErrors: true 
     }
   );
+
+  const config = new DocumentBuilder() 
+    .setTitle('Emprestimos API')
+    .setDescription('A Api de emprestimo de livros e materiais')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 
 bootstrap();
